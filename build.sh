351e8edaa8f05fa3dd0616de5daaf73134a1aefa
RyanWu9863 <ryan0611300@gmail.com>
2026-09-18 17:59:27 +0800

    測試改用開發設定執行，避開 HTTPS 強制轉址

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# 測試用獨立的 SQLite，不碰正式資料庫，也不需要建資料庫的權限
-DATABASE_URL="sqlite:///build-test.sqlite3" python manage.py test
+# 測試用開發環境的設定跑：獨立的 SQLite，且不套用強制 HTTPS 轉址
+DEBUG=True DATABASE_URL="sqlite:///build-test.sqlite3" python manage.py test
 
 python manage.py collectstatic --no-input
 python manage.py migrate
