840c3cf01894dc7f8567cefe2ec2ea379da216d3
RyanWu9863 <ryan0611300@gmail.com>
2026-09-18 17:55:52 +0800

    建置時先跑測試

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+# 測試用獨立的 SQLite，不碰正式資料庫，也不需要建資料庫的權限
+DATABASE_URL="sqlite:///build-test.sqlite3" python manage.py test
+
 python manage.py collectstatic --no-input
 python manage.py migrate
